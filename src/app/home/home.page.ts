4f4e8411713a35e0cd0caf8219b0bf877035acab
eaindra.naing <45616806+theednorca@users.noreply.github.com>
2023-03-29 08:43:52 +0630

    ins

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,22 @@
 import { Component } from '@angular/core';
-
+import { HttpClient } from '@angular/common/http';
+// import { FilesystemDirectory, Plugins } from '@capacitor/core';
+// const { Filesystem } = Plugins;
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  products: any[] = [];
+  constructor(private http: HttpClient){
+    this.loadProducts();
+  }
 
-  constructor() {}
+  loadProducts() {
+    this.http.get<any[]>('https://fakestoreapi.com/products').subscribe(res => {
+      this.products = res;
+    });
+  }
 
 }
